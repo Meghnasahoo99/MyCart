5be07b5c3cf3b1cb7bd0eb3e7dbab9e676dd7f43
Meghna sahoo <Meghna.sahoo27@gmail.com>
2024-08-03 15:22:50 +0530

    Adding Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            MyCart
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
